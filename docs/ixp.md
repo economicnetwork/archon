c6b80b194d66ca268d68867c475c98c59eeca698
benjyz <ben@enet.io>
2018-11-25 10:59:15 +0700

    docs

---
+++ docs/ixp.md
@@ -0,0 +1,21 @@
+# Internet trading protocol
+
+wip
+
+Unified data and API for exchange
+
+Market:
+
+* nominator_denomitor, e.g. LTC_BTC
+
+Orders:
+
+tbd
+
+Balances:
+
+tbd
+
+Transactions:
+
+tbd
--- examples/submit_order.py
+++ examples/submit_order.py
@@ -1,7 +1,6 @@
 import sys
 import archon.broker as broker
 import archon.arch as arch
-import archon.markets as markets
 import archon.model.models as m
 import archon.exchange.exchanges as exc
 from archon.util import *
@@ -14,29 +13,30 @@ from util import *
 
 import math
 
-abroker = broker.Broker()
-arch.setClientsFromFile(abroker)
+a = arch.Arch()
+ae = [exc.KUCOIN,exc.BITTREX,exc.CRYPTOPIA,exc.HITBTC]
+a.set_active_exchanges(ae)
+a.set_keys_exchange_file()
 
 def ordering(e):       
-    market = markets.get_market("ETH","BTC",e)
+    market = m.get_market("LTC","BTC",e)
+    b = a.abroker.balance_all(exchange=e)
+    print ("balance ",b)
     print ("market " + market)    
-    s = abroker.get_market_summary_str(market, e)
-    kb = m.bid_key(e)
-    ka = m.ask_key(e)
-    bid = s[kb]    
-    ask = s[ka]
+    s = a.abroker.get_market_summary(market, e)
+    print ("s ",s)
+    bid = s["bid"]    
+    ask = s["ask"]
     trade_type = "BUY"
-    pip = 0.00000001
     rho = 0.1
     price = bid * (1-rho)
-    qty =  1.23
+    qty =  0.01
     o = [market, trade_type, price, qty]
     print ("order " + str(o))
-    r = abroker.submit_order(o,e)
-    print ("result " + str(r))
+    #r = abroker.submit_order(o,e)
+    #print ("result " + str(r))
 
 if __name__=='__main__':
-    #e = exc.BITTREX
     es = [exc.BITTREX,exc.CRYPTOPIA]
     for e in es:
         ordering(e)
