59047a3c2610500bd9e43c525d018c3ee83b9002
benjyz <ben@enet.io>
2018-12-27 23:56:14 +0700

    bitmex

---
+++ archon/ws/bitmex/bitmex_test.py
@@ -0,0 +1,80 @@
+from bitmex_ws import BitMEXWebsocket
+import logging
+from logging.handlers import RotatingFileHandler
+from time import sleep
+import datetime
+import json
+import toml
+
+crypto = "XBTUSD"
+sleeping = 1
+
+def toml_file(fs):
+    with open(fs, "r") as f:
+        return f.read()
+
+def parse_toml(filename):
+    toml_string = toml_file(filename)
+    parsed_toml = toml.loads(toml_string)
+    return parsed_toml
+
+def run(k,s):
+    logger = setup_logger()
+
+    ws = BitMEXWebsocket(endpoint="https://www.bitmex.com/api/v1",
+                         symbol=crypto, api_key=k, api_secret=s)
+    logger.info("Instrument data: %s" % ws.get_instrument())
+    logger.info("\n\n\n\n************\n\n\n")
+    while(ws.ws.sock.connected):        
+        logger.info("Ticker: %s" % ws.get_ticker())
+
+        logger.info("Open orders: %s" % ws.open_orders(''))
+
+        t = ws.recent_trades()
+        logger.info("recent trades: %i" % len(t))
+
+        d = ws.market_depth()
+        logger.info("depth %i " % len(d))
+        sells = list(filter(lambda d: d['side'] == 'Sell' , d))        
+        buys = list(filter(lambda d: d['side'] == 'Buy' , d))        
+        #sells = list(filter(lambda d: d['side'] == 'Sell' , d))        
+        #logger.info(sells[0])
+        topbuys = buys[:5]
+        topsells = sells[-5:]
+        #logger.info(buys[-1])
+        logger.info("depth sells %i " % len(sells))
+        logger.info("depth  buys %i " % len(buys))
+
+        for i in range(5):
+            bp,bq = topbuys[i]['price'],topbuys[i]['size']
+            sp,sq = topsells[i]['price'],topsells[i]['size']
+            logger.info("%5.3f   %5.2f"%(bp,sp))
+
+        sleep(sleeping)
+
+def setup_logger():
+    # Prints logger info to terminal
+    logger = logging.getLogger()
+    # Change this to DEBUG if you want a lot more info
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    # create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler = RotatingFileHandler(
+        '_activity.log', 'a', 1000000, 1)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+if __name__ == "__main__":
+    
+    filename = "apikeys.toml"
+    apikeys = parse_toml(filename)['bitmex']
+    k,s = apikeys['key'],apikeys['secret']
+    run(k,s)
+
---
+++ archon/ws/bitmex/bitmex_topics.py
@@ -0,0 +1,34 @@
+
+#does not require auth
+TOPIC_announcement = "announcement"        #Site announcements
+TOPIC_chat = "chat"                #Trollbox chat
+TOPIC_connected = "connected"           #Statistics of connected users/bots
+TOPIC_funding = "funding"             #Updates of swap funding rates. Sent every funding interval (usually 8hrs)
+TOPIC_instrument = "instrument"          #Instrument updates including turnover and bid/ask
+TOPIC_insurance = "insurance"           #Daily Insurance Fund updates
+TOPIC_liquidation = "liquidation"         #Liquidation orders as they're entered into the book
+TOPIC_orderBookL2_25 = "orderBookL2_25"      #Top 25 levels of level 2 order book
+TOPIC_orderBookL2 = "orderBookL2"         #Full level 2 order book
+TOPIC_orderBook10 = "orderBook10"         #Top 10 levels using traditional full book push
+TOPIC_publicNotifications = "publicNotifications" #System-wide notifications (used for short-lived messages)
+TOPIC_quote = "quote"               #Top level of the book
+TOPIC_quoteBin1m = "quoteBin1m"          #1-minute quote bins
+TOPIC_quoteBin5m = "quoteBin5m"          #5-minute quote bins
+TOPIC_quoteBin1h = "quoteBin1h"          #1-hour quote bins
+TOPIC_quoteBin1d = "quoteBin1d"          #1-day quote bins
+TOPIC_settlement = "settlement"          #Settlements
+TOPIC_trade = "trade"               #Live trades
+TOPIC_tradeBin1m = "tradeBin1m"          #1-minute trade bins
+TOPIC_tradeBin5m = "tradeBin5m"          #5-minute trade bins
+TOPIC_tradeBin1h = "tradeBin1h"          #1-hour trade bins
+TOPIC_tradeBin1d = "tradeBin1d"          #1-day trade bins
+
+#requires auth
+TOPIC_affiliate = "affiliate"   # Affiliate status such as total referred users & payout %
+TOPIC_execution = "execution"   # Individual executions; can be multiple per order
+TOPIC_order = "order"       # Live updates on your orders
+TOPIC_margin = "margin"      # Updates on your current account balance and margin requirements
+TOPIC_position = "position"    # Updates on your positions
+TOPIC_privateNotifications = "privateNotifications" # Individual notifications - currently not used
+TOPIC_transact = "transact"     # Deposit/Withdrawal updates
+TOPIC_wallet = "wallet"       # Bitcoin address balance data including total deposits & withdrawals
