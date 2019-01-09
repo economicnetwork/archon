e3c107f1e6da9f0cf7060c0612f9f9b143eb9d5d
benjyz <ben@enet.io>
2019-01-09 21:34:14 +0700

    trades per sec

--- examples/bitmex_ticker.py
+++ examples/bitmex_ticker.py
@@ -7,13 +7,14 @@ import time
 import archon.orders as orders
 import archon.broker as broker
 import archon.exchange.exchanges as exc
+import datetime
 
 abroker = broker.Broker()
 abroker.set_active_exchanges([exc.BITMEX])
 
 client = abroker.afacade.get_client(exc.BITMEX)
 
-print ("len(buys), len(sells) , sumbuy, sumsell, lastprice, return , t1, t2")
+print ("len(buys), len(sells) , sumbuy, sumsell, lastprice, return , t1, t2, delta seconds, trades/second")
 print ('*'*20)
 while True:
     
@@ -31,6 +32,13 @@ while True:
     last = trades[0]
     t1 = first['timestamp']
     t2 = last['timestamp']
+    #'2019-01-09T14:29:10.136Z'
+    dt1 = datetime.datetime.strptime(t1[:-5],'%Y-%m-%dT%H:%M:%S')
+    dt2 = datetime.datetime.strptime(t2[:-5],'%Y-%m-%dT%H:%M:%S')
+    td = dt2-dt1
+
     ret = first['price']/last['price'] -1
-    print (len(buys),len(sells),sumbuy,sumsell,lastprice,ret,t1,t2)
+    sec = td.seconds
+    trade_sec = round(100/sec,3)
+    print (len(buys),len(sells),sumbuy,sumsell,lastprice,ret,dt1,dt2,td.seconds,trade_sec)
     time.sleep(5)
