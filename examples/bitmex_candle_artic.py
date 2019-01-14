7b696b7a51b4d9a8bca17749284809f26db73ad5
benjyz <ben@enet.io>
2019-01-14 12:44:32 +0700

    artic example

---
+++ examples/bitmex_candle_artic.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+import csv
+import sys
+import time
+
+import archon.orders as orders
+import archon.broker as broker
+import archon.exchange.exchanges as exc
+import archon.exchange.bitmex.bitmex as mex
+import datetime
+from loguru import logger
+
+from arctic import Arctic
+import quandl
+
+abroker = broker.Broker()
+abroker.set_active_exchanges([exc.BITMEX])
+
+client = abroker.afacade.get_client(exc.BITMEX)
+
+candles = client.trades_candle("XBTUSD", mex.candle_1d)
+candles.reverse()
+
+# Connect to Local MONGODB
+store = Arctic('localhost')
+
+# Create the library - defaults to VersionStore
+store.initialize_library('NASDAQ')
+
+# Access the library
+library = store['NASDAQ']
+
+library.write('XBTUSD', candles, metadata={'source': 'Bitmex'})
+
+# Reading the data
+item = library.read('XBTUSD')
+xbtusd = item.data
+#metadata = item.metadata
+
+print (xbtusd)
