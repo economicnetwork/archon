bc50cb8959d9dc3ecebb093a3227a4e18d22eacd
benjyz <ben@enet.io>
2019-02-09 11:25:07 +0700

    redis test

--- testing/test_redis.py
+++ testing/test_redis.py
@@ -4,16 +4,7 @@ redis simple test
 import redis
 from archon.config import *
 
-confFile = "conf.toml"
-try:
-    all_conf = parse_toml(confFile)
-except:
-    print("config file %s not properly formatted"%str(confFile))
-
-redis_conf = all_conf["REDIS"]       
-host = redis_conf["host"]     
-port = redis_conf["port"]
-redis_client = redis.Redis(host=host, port=port)
+redis_client = redis.Redis(host="localhost", port=6379)
 redis_client.set("test","test")
 x = redis_client.get("test")
 print (x)
