cdc441a671f2ee4af04833f2a14696694ec8fdcc
benjyz <ben@enet.io>
2019-04-07 13:11:10 +0700

    basic

---
+++ archon/exchange/bitkub.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+baseURL = "https://api.bitkub.com"
+
+class Client:
+
+    def __init__(self):
+        pass
+
+    def servertime(self):
+        url = baseURL + "/api/servertime"
+        result = requests.get(url).content
+        return result
+
+    def ticker(self):
+        url = baseURL + "/api/market/ticker"
+        result = requests.get(url).content
+        result = json.loads(result)
+        return result
+
+
+    def bids(self):
+        url = baseURL + "/api/market/bids"
+        payload = {"sym": "BTC", "lmt": 100}
+        result = requests.get(url, params=payload).content
+        return result
+
+if __name__=='__main__':
+    c = Client()
+    print (c.servertime())
+    #print (c.bids())
+    t = c.ticker()
+    for k,v in t.items():
+        print (k,v["last"])
