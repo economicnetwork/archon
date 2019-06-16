93035b9350bc724a2efbb009ad4bf0a34bca5391
benjyz <ben@enet.io>
2019-06-16 10:19:52 +0700

    binance balance example

--- examples/binance/balance_simple.py
+++ examples/binance/balance_simple.py
@@ -2,9 +2,11 @@
 balances simple
 """
 
-import archon.broker as broker
+import archon.broker.broker as broker
 import archon.exchange.exchanges as exc
 
+
+"""
 a = broker.Broker()
 a.set_active_exchanges([exc.BINANCE])
 bl = a.global_balances()
@@ -12,3 +14,9 @@ print (bl)
 for x in bl: 
 	for k,v in x.items(): 
 		print (k,":",v)
+"""
+
+a = broker.Broker(setAuto=False)
+a.set_keys_exchange_file(path_file_apikeys="./apikeys.toml")
+client = a.afacade.get_client(exc.BINANCE)
+print (client.get_account())
