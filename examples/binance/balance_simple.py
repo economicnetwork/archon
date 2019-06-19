4359aca78187f1a2138d6baf52c3b8f0a9b3082f
benjyz <ben@enet.io>
2019-06-19 15:42:32 +0700

    binance examples

--- examples/binance/balance_simple.py
+++ examples/binance/balance_simple.py
@@ -5,18 +5,13 @@ balances simple
 import archon.broker.broker as broker
 import archon.exchange.exchanges as exc
 
-
-"""
-a = broker.Broker()
-a.set_active_exchanges([exc.BINANCE])
-bl = a.global_balances()
-print (bl)
-for x in bl: 
-	for k,v in x.items(): 
-		print (k,":",v)
-"""
-
 a = broker.Broker(setAuto=False)
 a.set_keys_exchange_file(path_file_apikeys="./apikeys.toml")
 client = a.afacade.get_client(exc.BINANCE)
-print (client.get_account())
+bal = client.get_account()["balances"]
+for x in bal:
+	f,l = float(x["free"]),float(x["locked"])
+	a = x["asset"]
+	total = f+l
+	if total > 0:
+		print (a,total)
--- examples/binance/binance_stream.py
+++ examples/binance/binance_stream.py
@@ -15,7 +15,7 @@ bm = BinanceSocketManager(client)
 
 # This is our callback function. For now, it just prints messages as they come.
 def handle_message(msg):
-    print(msg['p'])
+    print("price ",msg['p'], " time ", msg['E'])
     #print(msg)
 
 # Start trade socket with 'ETHBTC' and use handle_message to.. handle the message.
@@ -23,10 +23,11 @@ def handle_message(msg):
 market = 'BTCUSDT'
 conn_key = bm.start_trade_socket(market, handle_message)
 # then start the socket manager
+print ("start stream")
 bm.start()
 
 # let some data flow..
-time.sleep(10)
+time.sleep(5)
 
 # stop the socket manager
 bm.stop_socket(conn_key)
