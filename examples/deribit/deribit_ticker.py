b3df34961c9ca6fed66dcb0044d83cec6b817827
benjyz <ben@enet.io>
2019-10-30 14:36:27 +0800

    fix deribit

--- examples/deribit/deribit_ticker.py
+++ examples/deribit/deribit_ticker.py
@@ -3,22 +3,31 @@ find options of deribit
 """
 
 from archon.exchange.deribit.Wrapper import DeribitWrapper
-import archon.config as config
 
 from datetime import datetime
 import time
 
-apikeys = config.parse_toml("apikeys.toml")
+import archon.exchange.exchanges as exc
+from archon.brokerservice.brokerservice import Brokerservice
+import archon.exchange.bitmex.bitmex as mex
+import time
+from util import *
 
-k = apikeys["DERIBIT"]["public_key"]
-s = apikeys["DERIBIT"]["secret"]
-w = DeribitWrapper(key=k,secret=s)
-time.sleep(5)
+broker = Brokerservice()
+
+def setup_broker():
+    user_id = parse_toml("conf.toml")["user_id"]
+    broker.set_apikeys_fromfile(user_id)
+    broker.activate_session(user_id)
+    broker.set_client(exc.DERIBIT)  
+    return broker
 
 if __name__=='__main__':  
-    print ("start....")              
+    print ("start....")
+    setup_broker()
+    deribit_client = broker.get_client(exc.DERIBIT)
     s = 'BTC-PERPETUAL'
-    summary = w.getsummary(s)
+    summary = deribit_client.getsummary(s)
     #'bidPrice': 3655.0, 'askPrice': 3655.5,
     bid,ask = summary['bidPrice'],summary['askPrice']
     print ("ticker: ",bid,ask)
---
+++ examples/deribit/util.py
@@ -0,0 +1,15 @@
+import toml
+from pathlib import Path
+
+def toml_file(fs):
+    try:
+        with open(fs, "r") as f:
+            r = f.read() 
+            return r           
+    except Exception as e:
+        raise Exception("toml file not found %s"%str(fs))
+
+def parse_toml(filename):
+    toml_string = toml_file(filename)
+    parsed_toml = toml.loads(toml_string)
+    return parsed_toml
